04986020072ab550009b29f1b8769fd2a1937813
iTelesyk <Glyof@yahoo.com>
2017-02-05 23:37:03 -0500

    Some changes made at work

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 
+# Heat treatment card magic numbers
 min_load_temp = 300 #F - minimal temperature before heat treatment should start
 cooling_end_temp = 400
 sr_nominal_temp = 1050 #F
@@ -20,20 +21,24 @@ index_buffer = 10
 def find_reference_points (df, column_name):
     ch_data = np.array(df[column_name])  # channel data
     heating_start_index = np.argmax(ch_data >= min_load_temp) - 1
-    print(ch_data[heating_start_index ], ' ', heating_start_index )
+    # print(ch_data[heating_start_index ], ' ', heating_start_index )
 
     sr_start_index = np.argmax(ch_data > sr_min_temp)
-    print(ch_data[sr_start_index ], ' ', sr_start_index)
+    # print(ch_data[sr_start_index ], ' ', sr_start_index)
 
     sr_data = ch_data[sr_start_index:]
     sr_end_index_relative = np.argmax(sr_data <= sr_min_temp)
     sr_end_index = sr_end_index_relative + sr_start_index
-    print(ch_data[sr_end_index],' ',sr_end_index)
+    # print(ch_data[sr_end_index],' ',sr_end_index)
 
     cooling_end_index_relative = np.argmax(sr_data < cooling_end_temp)
     cooling_end_index = cooling_end_index_relative + sr_start_index
-    print(ch_data[cooling_end_index],' ',cooling_end_index)
-    return;
+    # print(ch_data[cooling_end_index],' ',cooling_end_index)
+    reference_point_index_dict = {'heating_start_index':heating_start_index, 'sr_start_index':sr_start_index, 'sr_end_index':sr_end_index, 'cooling_end_index':cooling_end_index}
+    return reference_point_index_dict;
+
+
+
 
 
 #------------------Reading raw data from file------------------
@@ -60,19 +65,40 @@ del df[redundant_program_column_name]
 
 #------------------Data preparations------------------
 # Changing Excel Date/Time format to  Python datetime format
-# import xlrd
-# PyDate = pd.Series(df.Date)
-# #!!! that should be fixed -> it should be the way to treat it as a vector, not a list of individual number
-# for i in range(len(df.Date)):
-#     PyDate[i] = xlrd.xldate.xldate_as_datetime(df.Date[i], 0)  # Datemode = 0
-# df.Date = PyDate
-
-#Heat treatment card magic numbers
+import xlrd
+PyDate = pd.Series(df.Date)
+#!!! that should be fixed -> it should be the way to treat it as a vector, not a list of individual number
+for i in range(len(df.Date)):
+    PyDate[i] = xlrd.xldate.xldate_as_datetime(df.Date[i], 0)  # Datemode = 0
+df.Date = PyDate
 
 
-#Finding significant points on graph
-find_reference_points(df,'Top')
-find_reference_points(df,'Bottom')
+
+
+# Finding significant points on graph
+point_dict_top= find_reference_points(df, 'Top')
+point_dict_btn = find_reference_points(df, 'Bottom')
+
+# Finding heat rates
+date = np.array(df['Date'][point_dict_top['heating_start_index']: point_dict_top['sr_start_index']])
+heating = np.array(df['Top'][point_dict_top['heating_start_index']: point_dict_top['sr_start_index']])
+keep_going = True
+prev_timepoint = date[0]
+print(prev_timepoint)
+heat_rate_list = ()
+while keep_going:
+    timepoint = prev_timepoint + dt.timedelta(hours=1)
+    print(timepoint)
+    if timepoint > date[-1]:
+        keep_going = False
+    else:
+        prev_timepoint = timepoint
+
+# Finding stress relief duration
+sr_duration_top = dt.timedelta(df['Date'][point_dict_top['sr_end_index']] - df['Date'][point_dict_top['sr_start_index']])
+sr_duration_btn = dt.timedelta(df['Date'][point_dict_btn['sr_end_index']] - df['Date'][point_dict_btn['sr_start_index']])
+print('Stress relief duration (Top): ', sr_duration_top)
+print('Stress relief duration (Bottom): ', sr_duration_btn)
 
 # ------------------Plotting the data------------------
 #
